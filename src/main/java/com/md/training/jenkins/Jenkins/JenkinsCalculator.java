01f0f3337eaee82009b12f9df138bf1a7dfcf106
Vinit <mohini.dangale@gmail.com>
2020-01-21 22:47:45 +0530

    gfdgfd

---
+++ src/main/java/com/md/training/jenkins/Jenkins/JenkinsCalculator.java
@@ -0,0 +1,13 @@
+package com.md.training.jenkins.Jenkins;
+
+public class JenkinsCalculator {
+	//Method to add 2 numbers
+		public int addNumbers(int numberOne, int numberTwo) {
+			return numberOne + numberTwo;
+		}
+		
+		//Method 2 subtract 2 numbers
+		public int subtractNumbers(int numberOne, int numberTwo) {
+			return numberOne - numberTwo;
+		}
+}
